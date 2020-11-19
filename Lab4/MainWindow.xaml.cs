9c2ac21945bd746c4bc8f501c17fa17d09c0bd0c
kvd <kvd@omegasoftware.eu>
2020-11-19 23:50:37 +0300

    asd

---
+++ Lab4/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int m_X;
+        private int m_Y;
+        private double[,] data;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private double FindMax(double[] arr)
+        {
+            double res = double.MinValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (res < arr[i])
+                    res = arr[i];
+            }
+            return res;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int ii = 0; ii < m_X; ii++)
+                for (int i = 0; i < m_X - 1; i++)
+                {
+                    if (FindMax(data.GetRow(i)) > FindMax(data.GetRow(i + 1)))
+                    {
+                        for (int j = 0; j < m_Y; j++)
+                        {
+                            var item = data[i + 1, j];
+                            data[i + 1, j] = data[i, j];
+                            data[i, j] = item;
+                        }
+                    }
+                }
+            aaa.ItemsSource = data.AsTupleList();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            m_X = Convert.ToInt32(val1.Text);
+            m_Y = Convert.ToInt32(val2.Text);
+
+            var ran = new Random();
+            data = new double[m_X, m_Y];
+            for (int i = 0; i < m_X; i++)
+            {
+                for (int j = 0; j < m_Y; j++)
+                {
+                    data[i, j] = ran.Next(1, 100);
+                }
+            }
+            aaa.ItemsSource = data.AsTupleList();
+        }
+    }
+    public static class ArrayExtensions
+    {
+        public static T[] GetRow<T>(this T[,] data, int i)
+        {
+            return Enumerable.Range(0, data.GetLength(1)).Select(j => data[i, j]).ToArray();
+        }
+    }
+    public static class DataGridExtensions
+    {
+        public static List<object> AsTupleList<T>(this T[,] matrix)
+        {
+            var col = matrix.GetLength(1);
+            var result = new List<object>();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                T[] values = new T[col];
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    values[j] = matrix[i, j];
+                }
+
+                result.Add(GetTuple(values));
+            }
+
+            return result;
+        }
+        private static object GetTuple<T>(params T[] values)
+        {
+            Type genericType = Type.GetType("System.Tuple`" + values.Length);
+            Type[] typeArgs = values.Select(_ => typeof(T)).ToArray();
+            Type specificType = genericType.MakeGenericType(typeArgs);
+            object[] constructorArguments = values.Cast<object>().ToArray();
+            return Activator.CreateInstance(specificType, constructorArguments);
+        }
+
+    }
+}
---
+++ Lab8/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<EditPoly> EditPolies { get; set; }
+        public ObservableCollection<EditPolyValue> EditPoliesValue { get; set; }
+        public SeriesCollection SeriesCollection { get; set; }
+        public double[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            EditPolies = new List<EditPoly>
+            {
+                new EditPoly{ Title = "Start X:" },
+                new EditPoly{ Title = "Finish X:" },
+                new EditPoly{ Title = "Y: " },
+                new EditPoly{ Title = "Z:" },
+                new EditPoly{ Title = "H (Steps): " },
+            };
+            SeriesCollection = new SeriesCollection();
+            EditPoliesValue = new ObservableCollection<EditPolyValue>();
+            Labels = new[] { 1.0, 2.0, 2.0 };
+            YFormatter = value => value.ToString();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var listValue = CalcFun(EditPolies[0].Text, EditPolies[2].Text, EditPolies[3].Text, EditPolies[4].Text, EditPolies[1].Text);
+
+            SeriesCollection.Add(new LineSeries
+            {
+                Title = string.Format("Calc {0}", SeriesCollection.Count + 1),
+                Values = new ChartValues<double>(listValue)
+            });
+
+            listValue.Select((x, index) =>
+            {
+                var r = new EditPolyValue
+                {
+                    Title = string.Format("Value {0}", index),
+                    Text = x
+                };
+                r.PropertyChanged += check;
+                EditPoliesValue.Add(r);
+                return r;
+            }).ToList();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SeriesCollection.Clear();
+        }
+
+        private void check(object sender, PropertyChangedEventArgs e)
+        {
+            SeriesCollection.RemoveAt(SeriesCollection.Count - 1);
+            SeriesCollection.Add(new LineSeries
+            {
+                Title = string.Format("Calc {0}", SeriesCollection.Count + 1),
+                Values = new ChartValues<double>(EditPoliesValue.Select(x => x.Text))
+            });
+        }
+
+        private double Fun(double x, double y, double z)
+        {
+            var f1 = Math.Abs(Math.Pow(x, y / x) - Math.Pow(y / x, 1.0 / 3));
+            var f2 = (y - x) * ((Math.Cos(y) - (z / (y - x)) / 1 + Math.Pow(y - x, 2)));
+            return f1 + f2;
+        }
+
+        private List<double> CalcFun(double x, double y, double z, double h, double finish)
+        {
+            var res = new List<double>();
+            for (double i = x; i < finish; i += h)
+                res.Add(Fun(i, y, z));
+
+            var r = res.Where(x => !(double.IsNaN(x) || double.IsInfinity(x) || double.IsNegativeInfinity(x) || double.IsPositiveInfinity(x)));
+
+            var max = r.Max();
+            var min = r.Min();
+
+            var res1 = new List<double>();
+            foreach (var item in res)
+            {
+                if (double.IsNaN(item))
+                    res1.Add(0);
+                else if (double.IsInfinity(item))
+                    res1.Add(max * 3);
+                else if (double.IsPositiveInfinity(item))
+                    res1.Add(max * 3);
+                else if (double.IsNegativeInfinity(item))
+                    res1.Add(min * 3);
+                else
+                    res1.Add(item);
+            }
+            return res1;
+        }
+    }
+    public class EditPolyValue : VM
+    {
+        public string Title { get; set; }
+        private double m_Value;
+        public double Text
+        {
+            get { return m_Value; }
+            set
+            {
+                m_Value = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public class EditPoly : VM
+    {
+        public string Title { get; set; }
+        private double m_Value;
+        public double Text
+        {
+            get { return m_Value; }
+            set
+            {
+                m_Value = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public class VM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
